dddf4bf8a65a74869009530d140e3f2b4a256602
Jack-Anstey <jackajacka@outlook.com>
2022-12-11 16:03:12 -0500

    Changed the direction of the rank graphs

--- labelAnalysis.py
+++ labelAnalysis.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def graphLabels(label: pd.DataFrame, title: str) -> None:
+def graphLabels(label: pd.DataFrame, title: str, invert=False) -> None:
     """Make a bar chart of the labels to see if it's balanced or not
 
     Args:
         label (pd.DataFrame): the dataframe of labels that you want to graph
         title (str): The title of the graph
+        invert (bool): Should you invert the x axis direction? Default is False
     """
 
     labels, counts = np.unique(label, return_counts=True)
@@ -17,6 +18,8 @@ def graphLabels(label: pd.DataFrame, title: str) -> None:
     plt.legend(title=title)
     plt.xlabel("Label")
     plt.ylabel("Frequency of Label")
+    if invert:
+        plt.gca().invert_xaxis()
     plt.tight_layout()
     plt.show()
 
@@ -27,9 +30,9 @@ def main():
     rankReduced = pd.read_csv("pruned datasets/ranks-reduced.csv")
 
     graphLabels(popularity, "Popularity")
-    graphLabels(rank, "Peak Rank")
+    graphLabels(rank, "Peak Rank", True)
     graphLabels(popularityReduced, "Popularity Reduced")
-    graphLabels(rankReduced, "Peak Rank Reduced")
+    graphLabels(rankReduced, "Peak Rank Reduced", True)
 
 if __name__ == "__main__":
     main()
